2a07889db907e67aa843327bc2412da955f213e0
ddk9499 <ddk9499@gmail.com>
2020-01-22 03:45:45 +0500

    Clean up dependencies.

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -4,21 +4,11 @@
  * @author Dostonbek Kamalov (a.k.a @ddk9499)
  */
 
-object Versions {
-    const val kotlin = "1.3.60" // Don't forget to update in buildSrc/build.gradle.kts too!
-}
-
 object Libs {
 
-    val kotlin = Kotlin
     val ktorm = Ktorm
     val toothpick = Toothpick
     val network = Network
-
-    object Kotlin {
-        const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlin}"
-        const val coroutinesCore = "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.3"
-    }
 
     object Ktorm {
         private const val version = "2.6"
@@ -31,10 +21,7 @@ object Libs {
     }
 
     object Network {
-        private const val okHttpVersion = "4.2.2"
-
-        const val okHttp = "com.squareup.okhttp3:okhttp:$okHttpVersion"
-        const val logging = "com.squareup.okhttp3:logging-interceptor:$okHttpVersion"
+        const val okHttp = "com.squareup.okhttp3:okhttp:4.2.2"
     }
 
     val gson = "com.google.code.gson:gson:2.8.6"
